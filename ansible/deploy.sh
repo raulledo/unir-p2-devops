a0a2efdf730c64637005e21875a1b7efcd5e0f38
raulledo <info@raulledo.com>
2021-07-18 17:30:37 +0200

    U: Ansible > Creacion de estructura playbooks y roles nfs

--- ansible/deploy.sh
+++ ansible/deploy.sh
@@ -1,2 +1,3 @@
 ansible-playbook -i hosts configuracion-inicial-vms.yaml
+ansible-playbook -i hosts -l nfs instalacion-nfs.yaml
 ansible-playbook -i hosts -l master,workers tareas-comunes-master-workers.yaml
