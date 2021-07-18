cedd3d37da24a74a53d4e8c6eb416efe0edb2a12
raulledo <info@raulledo.com>
2021-07-18 18:53:57 +0200

    U: Ansible  > estructura playbooks

--- ansible/deploy.sh
+++ ansible/deploy.sh
@@ -1,3 +1,7 @@
 ansible-playbook -i hosts configuracion-inicial-vms.yaml
 ansible-playbook -i hosts -l nfs instalacion-nfs.yaml
 ansible-playbook -i hosts -l master,workers tareas-comunes-master-workers.yaml
+ansible-playbook -i hosts -l master configuracion-kubernetes.yaml
+ansible-playbook -i hosts instalacion-sdn.yaml
+ansible-playbook -i hosts -l master instalacion-ingress-controller.yaml
+ansible-playbook -i hosts -l master crear-usuario-no-admin.yaml
